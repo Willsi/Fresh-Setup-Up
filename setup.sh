4a8171a15a967e41c91c1cabbc313983af0b79df
Willsi <Willsi.cooper@googlemail.com>
2018-10-18 15:04:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ sudo apt-get install -y nano wget git vim python-pip
 sudo gdebi google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo gdebi google-chrome-stable_current_amd64.deb
-sudo gdebi google-chrome-stable_current_amd64.deb
+
 
